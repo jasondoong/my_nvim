2bc312aa22c1bf1951b6e857fdaec8f434927fd2
Jason Dong <jason@teaches.cc>
2026-01-20 13:50:34 +0800

    add goto definition and references hotkeys

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,6 @@
 vim.keymap.set('n', '<leader>e', function()
   require('mini.files').open(vim.api.nvim_buf_get_name(0), true)
 end, { desc = 'Open MiniFiles (current file dir)' })
+
+vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { desc = 'Go to definition' })
+vim.keymap.set('n', 'gr', vim.lsp.buf.references, { desc = 'Go to references' })
